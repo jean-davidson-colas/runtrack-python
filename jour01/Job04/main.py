83da2b39dc7d02a6bf7217c29e34d9399fe9e457
jean-davidson-colas <jean-davidson.colas@laplateforme.io>
2021-03-29 12:04:51 +0200

    Job 0 à 4

---
+++ jour01/Job04/main.py
@@ -0,0 +1,17 @@
+"""
+Enoncé:
+   Créez une fonction nommée Add. Cette fonction devra prendre 2 nombres
+   entiers en paramètres et retourner la somme de ces 2 entiers.
+   Depuis votre programme, appelez cette fonction plusieurs fois en y passant
+   des paramètres différents et affichez ces résultats.
+
+def nom_fonction(liste de paramètres):
+      bloc d'instructions"""
+
+
+def augmente_moi(a, b):
+    return 30 + a + b
+
+augmente_moi(1, 2)
+
+print(augmente_moi(1, 2))
